ab52aaef661aeb3363168cee238f70dec24b641d
ya2ha4 <>
2020-10-26 20:15:30 +0900

    インデントの修正

--- src/index.js
+++ src/index.js
@@ -7,57 +7,57 @@ import { Player, stringToDataUrl } from "textalive-app-api";
  */
 class Main {
     constructor() {
-            var canMng = new CanvasManager();
-            this._canMng = canMng;
+        var canMng = new CanvasManager();
+        this._canMng = canMng;
 
-            this._initPlayer();
+        this._initPlayer();
 
-            window.addEventListener("resize", () => this._resize());
-            this._update();
-        }
-        // プレイヤー初期化
+        window.addEventListener("resize", () => this._resize());
+        this._update();
+    }
+    // プレイヤー初期化
     _initPlayer() {
-            var player = new Player({
-                app: {
-                    appAuthor: "daniwell",
-                    appName: "Lyric Tiles"
-                },
-                mediaElement: document.querySelector("#media")
-            });
+        var player = new Player({
+            app: {
+                appAuthor: "daniwell",
+                appName: "Lyric Tiles"
+            },
+            mediaElement: document.querySelector("#media")
+        });
 
-            player.addListener({
-                onAppReady: (app) => this._onAppReady(app),
-                onVideoReady: (v) => this._onVideoReady(v),
-                onTimeUpdate: (pos) => this._onTimeUpdate(pos)
-            });
-            this._player = player;
-        }
-        // アプリ準備完了
+        player.addListener({
+            onAppReady: (app) => this._onAppReady(app),
+            onVideoReady: (v) => this._onVideoReady(v),
+            onTimeUpdate: (pos) => this._onTimeUpdate(pos)
+        });
+        this._player = player;
+    }
+    // アプリ準備完了
     _onAppReady(app) {
-            if (!app.songUrl) {
-                this._player.createFromSongUrl("https://www.youtube.com/watch?v=-6oxY-quTOA");
-            }
+        if (!app.songUrl) {
+            this._player.createFromSongUrl("https://www.youtube.com/watch?v=-6oxY-quTOA");
+        }
 
-            // 画面クリックで再生／一時停止
-            document.getElementById("view").addEventListener("click", () => function(p) {
-                if (p.isPlaying) p.requestPause();
-                else p.requestPlay();
-            }(this._player));
-        }
-        // ビデオ準備完了
+        // 画面クリックで再生／一時停止
+        document.getElementById("view").addEventListener("click", () => function(p) {
+            if (p.isPlaying) p.requestPause();
+            else p.requestPlay();
+        }(this._player));
+    }
+    // ビデオ準備完了
     _onVideoReady(v) {
-            // 歌詞のセットアップ
-            var lyrics = [];
-            if (v.firstChar) {
-                var c = v.firstChar;
-                while (c) {
-                    lyrics.push(new Lyric(c));
-                    c = c.next;
-                }
+        // 歌詞のセットアップ
+        var lyrics = [];
+        if (v.firstChar) {
+            var c = v.firstChar;
+            while (c) {
+                lyrics.push(new Lyric(c));
+                c = c.next;
             }
-            this._canMng.setLyrics(lyrics);
         }
-        // 再生位置アップデート
+        this._canMng.setLyrics(lyrics);
+    }
+    // 再生位置アップデート
     _onTimeUpdate(position) {
         this._position = position;
         this._updateTime = Date.now();
@@ -134,27 +134,27 @@ class CanvasManager {
 
     // 歌詞の更新
     setLyrics(lyrics) {
-            this._lyrics = lyrics;
-        }
-        // 再生位置アップデート
+        this._lyrics = lyrics;
+    }
+    // 再生位置アップデート
     update(position) {
-            // // マウスが画面外の時、オートモード
-            // if (!this._isOver) {
-            //     this._rx = Math.sin(position / 1234 + 0.123) * 0.3 + 0.2;
-            //     this._ry = Math.cos(position / 1011 + 0.111) * 0.5;
-            //     this._mouseX = this._stw * (this._rx + 1) / 2;
-            //     this._mouseY = this._sth * (this._ry + 1) / 2;
-            // }
-            // マウス位置に応じてスクロール位置の更新
-            var delta = (position - this._position) / 1000;
-            this._py += this._speed / 1000;
+        // // マウスが画面外の時、オートモード
+        // if (!this._isOver) {
+        //     this._rx = Math.sin(position / 1234 + 0.123) * 0.3 + 0.2;
+        //     this._ry = Math.cos(position / 1011 + 0.111) * 0.5;
+        //     this._mouseX = this._stw * (this._rx + 1) / 2;
+        //     this._mouseY = this._sth * (this._ry + 1) / 2;
+        // }
+        // マウス位置に応じてスクロール位置の更新
+        var delta = (position - this._position) / 1000;
+        this._py += this._speed / 1000;
 
-            this._drawBg();
-            this._drawLyrics();
+        this._drawBg();
+        this._drawLyrics();
 
-            this._position = position;
-        }
-        // リサイズ
+        this._position = position;
+    }
+    // リサイズ
     resize() {
         this._can.width = this._stw = document.documentElement.clientWidth;
         this._can.height = this._sth = document.documentElement.clientHeight;
@@ -162,62 +162,62 @@ class CanvasManager {
 
     // "mousemove" / "touchmove"
     _move(e) {
-            var mx = 0;
-            var my = 0;
+        var mx = 0;
+        var my = 0;
 
-            if (e.touches) {
-                mx = e.touches[0].clientX;
-                my = e.touches[0].clientY;
-            } else {
-                mx = e.clientX;
-                my = e.clientY;
-            }
-            this._mouseX = mx;
-            this._mouseY = my;
-            console.log("mx : " + mx);
-            console.log("my : " + my);
-
-            this._rx = (mx / this._stw) * 2 - 1;
-            this._ry = (my / this._sth) * 2 - 1;
-
-            this._isOver = true;
+        if (e.touches) {
+            mx = e.touches[0].clientX;
+            my = e.touches[0].clientY;
+        } else {
+            mx = e.clientX;
+            my = e.clientY;
         }
-        // "mouseleave" / "touchend"
+        this._mouseX = mx;
+        this._mouseY = my;
+        console.log("mx : " + mx);
+        console.log("my : " + my);
+
+        this._rx = (mx / this._stw) * 2 - 1;
+        this._ry = (my / this._sth) * 2 - 1;
+
+        this._isOver = true;
+    }
+    // "mouseleave" / "touchend"
     _leave(e) {
         this._isOver = false;
     }
 
     // 背景の模様描画
     _drawBg() {
-            var space = this._space;
+        var space = this._space;
 
-            var ox = this._px % space;
-            var oy = this._py % space;
+        var ox = this._px % space;
+        var oy = this._py % space;
 
-            var nx = this._stw / space + 1;
-            var ny = this._sth / space + 1;
+        var nx = this._stw / space + 1;
+        var ny = this._sth / space + 1;
 
-            var ctx = this._ctx;
-            ctx.clearRect(0, 0, this._stw, this._sth);
-            ctx.strokeStyle = "#000";
-            ctx.lineWidth = 1;
-            ctx.beginPath();
+        var ctx = this._ctx;
+        ctx.clearRect(0, 0, this._stw, this._sth);
+        ctx.strokeStyle = "#000";
+        ctx.lineWidth = 1;
+        ctx.beginPath();
 
-            for (var y = 0; y <= ny; y++) {
-                for (var x = 0; x <= nx; x++) {
-                    var tx = x * space + ox;
-                    var ty = y * space + oy;
+        for (var y = 0; y <= ny; y++) {
+            for (var x = 0; x <= nx; x++) {
+                var tx = x * space + ox;
+                var ty = y * space + oy;
 
-                    // 十字の模様描画
-                    ctx.moveTo(tx - 8, ty);
-                    ctx.lineTo(tx + 8, ty);
-                    ctx.moveTo(tx, ty - 8);
-                    ctx.lineTo(tx, ty + 8);
-                }
+                // 十字の模様描画
+                ctx.moveTo(tx - 8, ty);
+                ctx.lineTo(tx + 8, ty);
+                ctx.moveTo(tx, ty - 8);
+                ctx.lineTo(tx, ty + 8);
             }
-            ctx.stroke();
         }
-        // 歌詞の描画
+        ctx.stroke();
+    }
+    // 歌詞の描画
     _drawLyrics() {
         if (!this._lyrics) return;
         var position = this._position;
